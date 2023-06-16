359b2a83645229db9c20954df510a9b89d24d6c2
anjankow <anjankow@protonmail.com>
2023-06-16 08:51:37 +0000

    add template config

--- pkg/manager/template_database.go
+++ pkg/manager/template_database.go
@@ -1,5 +1,15 @@
 package manager
 
+type TemplateConfig struct {
+	TemplateHash string         `json:"templateHash"`
+	Config       DatabaseConfig `json:"config"`
+	nextTestID   int
+}
+
+func (c TemplateConfig) IsEmpty() bool {
+	return c.TemplateHash == ""
+}
+
 type TemplateDatabase struct {
 	Database `json:"database"`
 
